8c57934d3599425f9f8b8366011ef16a72e09cc6
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2023-03-29 23:06:53 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,83 @@
+// let min = prompt('Введите min:');
+// let max = prompt('Введите max:');
+
+// function getRandomNumber(min, max) {
+// min = Math.ceil(min)
+// max = Math.floor(max)
+// return Math.floor(Math.random() * (max - min) + min)
+// }
+
+// function getNumber(func) {
+// let count = 0;
+// let num = func;
+// while(num < 1000) {
+// console.log(num = (num * 3));
+// count++;
+// }
+// return {num, count};
+// }
+
+// 3
+
+// for (let i = 100; i >= 0; i--) {
+// console.log(i);
+// }
+
+//4
+
+// let num = 2;
+// let count = 0;
+// while (num < 1000) {
+// num *= 3;
+// count++
+// }
+// console.log(num);
+
+// const sum = Array.from({length: 50}, (_, i) => 2 + i * 2).reduce((acc, num) => acc + num, 0);
+// console.log(sum)
+
+// let stringNum = "-";
+// for (let i = 1; i < 10; i++) {
+// stringNum += `${i}-`;
+// }
+// console.log(stringNum);
+
+// for (let i = 0; i < arr.length; i++) {
+// if (arr[i] % 2 === 0) {
+// console.log(Math.pow(arr[i],2));
+// } else if (arr[i] % 3 === 0) {
+// console.log(Math.pow(arr[i],3));
+// }
+// const arr3 = [1, 2, 3, 4, 5];
+// const arr4 = arr3.splice(1,2);
+// console.log(arr3);
+
+// for (let i = 1; i < 99; i++) {
+// if (i % 2 === 0) {
+// console.log(i);
+// }
+
+// задача 1
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < array.length; i++) {
+  if (array[i] === 0) {
+    console.log(`${array[i]} – это ноль`);
+  } else if (array[i] % 2 === 0) {
+    console.log(`${array[i]} – четное число`);
+  } else {
+    console.log(`${array[i]} – нечетное число`);
+  }
+}
+// задача 1
+
+// задача 2
+
+// задача 2
+
+// задача 3
+
+// задача 3
+
+// задача 4
+
+// задача 4
